2f4ec71ca7e99994ad51e1d915e0a39153468bb5
angelinn <angelin.nedelchev@outlook.com>
2018-05-12 14:42:12 +0300

    Remove stdout buffering.

--- src/WebAPI/PartyOrganizer.WebAPI/main.cpp
+++ src/WebAPI/PartyOrganizer.WebAPI/main.cpp
@@ -1,6 +1,7 @@
 #include <silicon/api.hh>
 #include <silicon/backends/mhd.hh>
 #include "symbols.hh"
+#include <cstdio>
 
 using namespace sl; // Silicon namespace
 using namespace s; // Symbols namespace
@@ -14,7 +15,9 @@ auto hello_api = http_api(
 
 int main()
 {
+	setbuf(stdout, NULL);
 	printf("Starting server...");
+
 	// Serve hello_api via microhttpd using the json format:
 	sl::mhd_json_serve(hello_api, 12345);
 }
