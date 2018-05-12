7974b52fda5822daa79a1f7d4c17271b1cea04ae
angelinn <angelin.nedelchev@outlook.com>
2018-05-12 14:13:08 +0300

    Add hello world api code and symbols.hh.

--- src/WebAPI/PartyOrganizer.WebAPI/main.cpp
+++ src/WebAPI/PartyOrganizer.WebAPI/main.cpp
@@ -1,6 +1,20 @@
-#include <cstdio>
+#include <silicon/api.hh>
+#include <silicon/backends/mhd.hh>
+#include "symbols.hh"
+
+using namespace sl; // Silicon namespace
+using namespace s; // Symbols namespace
+
+				   // Define the API:
+auto hello_api = http_api(
+
+	// The hello world procedure.
+	GET / _hello = []() { return D(_message = "Hello world."); }
+);
 
 int main()
 {
-	printf("Hello world!\n");
+	printf("Starting server...");
+	// Serve hello_api via microhttpd using the json format:
+	sl::mhd_json_serve(hello_api, 12345);
 }
