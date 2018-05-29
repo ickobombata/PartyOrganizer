36f2554929d45cf99c09e56efe255f8a5a3a01a2
angelinn <angelin.nedelchev@outlook.com>
2018-05-29 23:06:38 +0300

    Add build settings for jwt library.

--- src/WebAPI/PartyOrganizer.WebAPI/main.cpp
+++ src/WebAPI/PartyOrganizer.WebAPI/main.cpp
@@ -2,6 +2,7 @@
 #include <silicon/backends/mhd.hh>
 #include "symbols.hh"
 #include <cstdio>
+#include <jwt/jwt_all.h>
 
 using namespace sl; // Silicon namespace
 using namespace s; // Symbols namespace
@@ -13,10 +14,27 @@ auto hello_api = http_api(
 	GET / _hello = []() { return D(_message = "Hello world."); }
 );
 
+void GenerateToken(const std::string& username, const std::string& password)
+{
+	using json = nlohmann::json;
+
+	// Setup a signer
+	HS256Validator signer("secret!");
+
+	// Create the json payload that expires 01/01/2017 @ 12:00am (UTC)
+	json payload = { { "sub", "subject" },{ "exp", 1483228800 } };
+
+	// Let's encode the token to a string
+	auto token = JWT::Encode(signer, payload);
+
+	printf("Token %s\n", token.c_str());
+}
+
 int main()
 {
 	setbuf(stdout, NULL);
 	printf("Starting server...");
+	GenerateToken(std::string(), std::string());
 
 	// Serve hello_api via microhttpd using the json format:
 	sl::mhd_json_serve(hello_api, 12345);
