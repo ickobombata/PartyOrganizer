b797f9e501d7180d8ac88b29cc08b56a4ea81ec5
angelinn <angelin.nedelchev@outlook.com>
2018-06-04 23:37:29 +0300

    Add simple login react page.

---
+++ src/Mobile/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Login from './Login';
+
+export default class App extends React.Component {
+  state = {
+    isLoggedIn: false
+  }
+
+  render() {
+    return (
+      <Login onLoginPress={() => this.setState({isLoggedIn: true})} />
+    );
+  }
+}
