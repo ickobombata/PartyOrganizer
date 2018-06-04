78ce4d594a79062b6e2ed27810e8fdc0ca78b0ea
angelinn <angelin.nedelchev@outlook.com>
2018-06-04 22:38:18 +0300

    Add token validation logic.

--- src/WebAPI/PartyOrganizer.WebAPI/Services/TokenService.cpp
+++ src/WebAPI/PartyOrganizer.WebAPI/Services/TokenService.cpp
@@ -26,17 +26,25 @@ std::string TokenService::GenerateToken(const std::string& username, const std::
 
 bool TokenService::ValidateToken(const std::string& token)
 {
-	return true;
-	//try
-	//{
-	//	ExpValidator exp;
-	//	json header, payload;
+	using namespace jwt::params;
 
-	//	std::tie(header, payload) = JWT::Decode(token, &signer, &exp);
-	//	return true;
-	//}
-	//catch (InvalidTokenError& ex)
-	//{
-	//	return false;
-	//}
+	try
+	{
+		jwt::jwt_object obj = jwt::decode(token, algorithms({ "hs256" }), secret(signingSecret), issuer(ISSUER));
+	}
+	catch (const jwt::TokenExpiredError& e)
+	{
+		return false;
+	}
+	catch (const jwt::InvalidIssuerError& e)
+	{
+		return false;
+	}
+	catch (const std::runtime_error& e)
+	{
+		const char* wut = e.what();
+		return false;
+	}
+
+	return true;
 }
